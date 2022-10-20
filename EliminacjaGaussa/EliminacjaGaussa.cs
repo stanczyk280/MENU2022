342ccbba579f1d3cdbc164eace8a9f4e7992d04f
Bartosz "Stańczyk" Grabowski <47967674+stanczyk280@users.noreply.github.com>
2022-10-20 11:44:33 +0200

    +Eliminacja gaussa

---
+++ EliminacjaGaussa/EliminacjaGaussa.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliminacjaGaussa
+{
+    internal class EliminacjaGaussa
+    {
+        public static double[,] StworzMacierz(double[,] macierz)
+            => new double[macierz.Length, macierz.Length];
+        public static double[,] KopiujMaicerz(double[,] macierz)
+        {
+            var n = macierz.Length;
+            double[,] macierzWynikowa = StworzMacierz(macierz);
+            for(var i=0; i<n;i++)
+            {
+                for(var j=0; j<n; j++)
+                {
+                    macierzWynikowa[i, j] = macierz[i, j];
+                }
+            }
+            return macierzWynikowa;
+        }
+
+        public static double[] KopiujWektor(double[] wektor)
+        {
+            double[] kopiaWektor = new double[wektor.Length];
+            for(int i=0; i<wektor.Length; i++)
+            {
+                kopiaWektor[i] = wektor[i];
+            }
+            return kopiaWektor;
+        }
+
+        public static void Wyswietl(double[,] macierz)
+        {
+            var n = macierz.Length;
+            for (var i = 0; i < n; i++)
+            {
+                for (var k = 0; k < n; k++)
+                {
+                    Console.Write(macierz[i, k] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public static double[] Eliminacja(double[,] macierz, double[] wektor)
+        {
+            var n = macierz.Length;
+            double[,] macierzWynikowa = KopiujMaicerz(macierz);
+            double[] wektorKopia = KopiujWektor(wektor);
+
+            for(int i=1;i<=n-1;i++)
+            {
+                for (int j = i + 1; j <= n; j++)
+                {
+                    for (int k = i + 1; k <= n; k++)
+                    {
+                        macierzWynikowa[j,k] = macierz[j,k] - (macierz[i,k] * (macierz[j,i] / macierz[i,i]));
+                    }
+                    wektorKopia[j] = wektor[j] - (wektor[i] * (macierz[j,i] / macierz[i,i]));
+                    for (int ii = 0; ii < macierzWynikowa.Length; ii++)
+                    {
+                        for (int jj = 0; jj < macierzWynikowa.Length; jj++)
+                        {
+                            macierz[ii,jj] = macierzWynikowa[ii,jj];
+                        }
+                        wektor[ii] = wektorKopia[ii];
+                    }
+                }
+            }
+            return wektor;
+        }
+    }
+}
---
+++ MacierzOdwrotna/MacierzOdwrotna.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacierzOdwrotna
+{
+    internal class MacierzOdwrotna
+    {
+        public static double[,] StworzMacierz(int rows, int cols)
+            => new double[rows, cols];
+
+        public void WypiszMacierz(int rows, int cols, int[,] macierz)
+        {
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < cols; j++)
+                {
+                    Console.Write(macierz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void PrzemnozMacierze(int m1Rows, int m1Cols, int m2Cols, int[,] macierz1, int[,] macierz2)
+        {
+            try
+            {
+                int[,] macierzWynikowa = new int[m1Rows, m2Cols];
+                for (var i = 0; i < m1Rows; i++)
+                {
+                    for (var j = 0; j < m2Cols; j++)
+                    {
+                        macierzWynikowa[i, j] = 0;
+                        for (var k = 0; k < m1Cols; k++)
+                        {
+                            macierzWynikowa[i, j] += macierz1[i, k] * macierz2[k, j];
+                        }
+                    }
+                }
+                WypiszMacierz(m1Rows, m2Cols, macierzWynikowa);
+            }
+            catch(Exception ex)
+            {
+                ex = new InvalidOperationException();
+                Console.WriteLine("Mnozenie macierzy jest niemozliwe",ex);
+            }
+        }
+
+        //public static double[,] OdwrocMacierz(double[,] macierz)
+        //{
+        //    int n = macierz.Length;
+        //    double[,] wynik = StworzMacierz(n, n);
+        //    for(var i = 0; i < n; i++)
+        //    {
+        //        for(var j = 0; j < n; j++)
+        //        {
+        //            wynik[i, j] = macierz[i, j];
+        //        }
+        //    }    
+        //}
+    }
+}
